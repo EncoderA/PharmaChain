4fb755bf5eb3993125ba17e1327708a2b83adead
Ankush Khairnar <ankushkhairnar12@gmail.com>
2025-09-30 19:22:38 +0530

    dashboard layout and login page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,60 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AnimatedThemeToggler } from "@/components/ui/theme-toggle";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2">
+              <div className="w-full flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <div className="flex justify-between gap-3 items-center w-full">
+                  <Breadcrumb>
+                    <BreadcrumbList>
+                      <BreadcrumbItem className="hidden md:block">
+                        <BreadcrumbLink href="#">
+                          Building Your Application
+                        </BreadcrumbLink>
+                      </BreadcrumbItem>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                      <BreadcrumbItem>
+                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                      </BreadcrumbItem>
+                    </BreadcrumbList>
+                  </Breadcrumb>
+                  <AnimatedThemeToggler />
+                </div>
+              </div>
+            </header>
+            {children}
+          </SidebarInset>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import * as React from "react"
+import {
+  Command,
+  Frame,
+  Map,
+  PieChart,
+  Receipt,
+  Settings,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { NavbarLogo } from "./ui/resizable-navbar"
+import Image from "next/image"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+     {
+      title: "Dashboard",
+      name: "Dashboard",
+      url: "#",
+      icon: Frame,
+    },
+    {
+      title: "Products",
+      name: "Products",
+      url: "#",
+      icon: PieChart,
+    },
+    {
+      title: "Transactions",
+      name: "Transactions",
+      url: "#",
+      icon: Receipt,
+    },
+    {
+      title: "Reports",
+      name: "Reports",
+      url: "#",
+      icon: Map,
+    },
+    {
+      title: "Settings",
+      name: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ],
+ 
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className=" flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Image
+                    src="/logo.svg"
+                    alt="PharmaChain"
+                    width={32}
+                    height={32}
+                  />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">PharmaChain</span>
+                  <span className="truncate text-xs">Manufactuer</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
